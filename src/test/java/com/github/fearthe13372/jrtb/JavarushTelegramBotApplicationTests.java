ce8ce53a3b903eb098a3176b919b335f4bc52170
Egor Rakhimov <agent00753545@gmail.com>
2023-03-15 15:30:24 +0300

    JRTB-0 add SpringBoot skeleton to the project

---
+++ src/test/java/com/github/fearthe13372/jrtb/JavarushTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fearthe13372.jrtb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavarushTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
